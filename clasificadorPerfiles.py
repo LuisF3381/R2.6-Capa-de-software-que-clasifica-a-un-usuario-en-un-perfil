cf0e0231c42a606576a47df49bfb994d39ada596
luisf3382 <124219151+luisf3382@users.noreply.github.com>
2023-09-25 16:19:17 -0500

    Procesamiento de las encuestas
    Se ha terminado de procesar las encuestas para trasnformarlas en un arreglo

--- clasificadorPerfiles.py
+++ clasificadorPerfiles.py
@@ -8,22 +8,28 @@ def leer_archivo_encuesta():
     df = pd.read_excel(archivo, engine='openpyxl', header=None)
 
     list_respuestas = []
-    for index, row in df.iterrows():
+    for index, row in df.iloc[1:].iterrows(): # Ignoramos las filas de la cabecera
         # Aquí 'row' es una Serie con los valores de la fila actual.
         # Puedes acceder a los valores por su índice numérico.
         lista_aux = []
         # El arreglo tendra la estructura
-        # Edad, Habilidades con productos tecnológicos, Frecuencia, Tipo de transaccion, Tamaño de fuente, Diseño de pantalla
-        lista_aux.append(row[2])
-        lista_aux.append(row[3])
-        lista_aux.append(row[4])
-        lista_aux.append(row[6])
-        lista_aux.append(row[7])
-        lista_aux.append(row[9])
-
+        lista_aux.append(row[2]) # Edad
+        lista_aux.append(row[3]) # Habilidades con productos tecnológicos
+        lista_aux.append(row[4]) # Frecuencia de uso
+        lista_aux.append(row[6]) # Tipo de transaccion
+        lista_aux.append(row[7]) # Tamaño de fuente
+        lista_aux.append(row[9]) # Diseño de pantalla
+        # Metricas de interaccion Positivas
+        lista_aux.append(row[11]) # Rapidez de las transacciones
+        lista_aux.append(row[12]) # Facilidad de uso
+        lista_aux.append(row[13]) # Intuitividad en la interfaz de uso
+        # Metricas de interaccion Negativas
+        lista_aux.append(row[17]) # Dificultad en la navegación por las opciones en pantalla
+        lista_aux.append(row[18]) # Confusión causada por la falta de claridad en los iconos y símbolos
+        lista_aux.append(row[19]) # Problemas para encontrar la información deseada en la pantalla
+        lista_aux.append(row[20]) # Lentitud en la respuesta de la interfaz a tus acciones}
         # Finalmente lo añadimos al arreglo general
         list_respuestas.append(lista_aux)
-        print(lista_aux)
 
     return list_respuestas
 
@@ -35,7 +41,6 @@ def clasificador_perfiles(arr_respuestas):
 def main():
     # Se preprocesa el archivo de encuesta de los usuarios de cajeros ATM
     respuestas_usuarios = leer_archivo_encuesta()
-
     # Se aplica el clasificador basado en reglas para clasificar a los usuarios
     usuarios_clasificados = clasificador_perfiles(respuestas_usuarios)
 
